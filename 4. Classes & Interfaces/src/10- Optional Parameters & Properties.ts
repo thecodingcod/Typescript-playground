82f4e903e73d3d3748702fd84536e3138afdbba9
Mohamed Benali <mohamedalihalawa@hotmail.com>
2020-10-12 10:07:57 +0200

    fixed Optional Parameters & Properties Example

--- 4. Classes & Interfaces/src/10- Optional Parameters & Properties.ts
+++ 4. Classes & Interfaces/src/10- Optional Parameters & Properties.ts
@@ -1,23 +1,78 @@
-interface INameables {
-  readonly name: string;
+interface IDevice {
+  Name: string;
+  Price: number;
+}
+interface IComputing {
+  Screen: IDevice;
+  Mouse: IDevice;
+  Keyboard: IDevice;
+  Kase: IDevice;
+  GetPrice(): number;
 
-  // optional
-  outputNames?: string;
+  // Optional Parameteres and properties
+  Barcode?: IDevice;
+  ScanBarCode?(barcode: any): string;
 }
 
-class xyz implements INameables {
-    
-    // constructor
-    constructor(name: string) {
-        this.name = name;
-    }
-    name: string;
+class Computer implements IComputing {
+  private screen: IDevice;
+  private mouse: IDevice;
+  private keyboard: IDevice;
+  private kase: IDevice;
+  private barcode?: IDevice;
 
- // properties
-    get Name(): string {
-      return this.name;
+  constructor(
+    screen: IDevice,
+    mouse: IDevice,
+    keyboard: IDevice,
+    kase: IDevice,
+    barcode?: IDevice
+  ) {
+    this.screen = screen;
+    this.mouse = mouse;
+    this.keyboard = keyboard;
+    this.kase = kase;
+    if (barcode) {
+      this.barcode = barcode;
     }
+  }
 
-    // optional field
-    outputNames?: string | undefined;
+  get Screen(): IDevice {
+    return this.screen;
+  }
+  get Keyboard(): IDevice {
+    return this.keyboard;
+  }
+  get Mouse(): IDevice {
+    return this.mouse;
+  }
+  get Kase(): IDevice {
+    return this.kase;
+  }
+
+  // commenting this won't lead to any problems!
+  //   get Barcode(): IDevice | undefined {
+  //     if (this.barcode) return this.barcode;
+  //     else return undefined;
+  //   }
+
+  GetPrice(): number {
+    let price: number =
+      this.Mouse.Price +
+      this.Keyboard.Price +
+      this.Kase.Price +
+      this.screen.Price;
+    if (this.barcode) price += this.barcode.Price;
+    return price;
+  }
 }
+
+let computer = new Computer(
+  { Name: "HP LCD", Price: 1500 },
+  { Name: "Laser Mouse", Price: 45 },
+  { Name: "Machine Gun", Price: 450 },
+  { Name: "Fujitsu", Price: 4500 }
+  //   { Name: "Barcode 2019", Price: 505 }
+);
+
+console.log(computer.GetPrice());
